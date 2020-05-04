e7e5e52cb8bd3c8fdefacf12b45f5bc91de7ddf2
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-05-04 11:13:58 -0400

    Clean up code style per pycodestyle, pydocstyle, and pylint.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,22 +1,46 @@
-import boto3
-import click
-import mimetypes
-from botocore.exceptions import ClientError
-from pathlib import Path
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-session = boto3.Session(profile_name='pythonAutomation')
-s3 = session.resource('s3')
+"""Webotron: Deploy websites with AWS.
+
+Webtoron automates the process of deploying static websites to AWS.
+- Configure AWS S3 buckets
+    - Create them
+    - Set them up for static website hosting
+    - Deploy local files to them
+- Configure DNS with AWS Route 53
+- Configure a Content Delivery Network and SSL with AWS CloudFront
+"""
+
+from pathlib import Path
+import mimetypes
+
+import boto3
+from botocore.exceptions import ClientError
+import click
+from bucket import BucketManager
+
+session = None
+bucket_manager = None
 
 
 @click.group()
-def cli():
-    """webotron deploys websites to AWS"""
-    pass
+@click.option('--profile', default=None, help="Use a given AWS profile.")
+def cli(profile):
+    """Webotron deploys websites to AWS."""
+    global session_cfg, bucket_manager
+    session_cfg = {}
+
+    if profile:
+        session_cfg['profile_name'] = profile
+
+    session = boto3.Session(**session_cfg)
+    bucket_manager = BucketManager(session)
 
 
 @cli.command('list-buckets')
 def list_buckets():
-    """List all s3 buckets"""
+    """List all s3 buckets."""
     for bucket in s3.buckets.all():
         print(bucket)
 
@@ -24,7 +48,7 @@ def list_buckets():
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    """List objects in an s3 bucket"""
+    """List objects in an s3 bucket."""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
 
@@ -32,21 +56,22 @@ def list_bucket_objects(bucket):
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    """Create and configure s3 bucket"""
+    """Create and configure s3 bucket."""
     s3_bucket = None
 
     try:
         s3_bucket = s3.create_bucket(
             Bucket=bucket
         )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             # prints error that bucket is already owned by user
             s3_bucket = s3.Bucket(bucket)
         else:
             # raises error
-            raise e
+            raise error
 
+    # Security policy for S3 Buckets
     policy = """
     {
         "Version": "2012-10-17",
@@ -66,8 +91,7 @@ def setup_bucket(bucket):
     pol = s3_bucket.Policy()
     pol.put(Policy=policy)  # adds policy to bucket
 
-    ws = s3_bucket.Website()
-    ws.put(WebsiteConfiguration={
+    s3_bucket.Website().put(WebsiteConfiguration={
         'ErrorDocument': {
             'Key': 'error.html'
         },
@@ -80,12 +104,15 @@ def setup_bucket(bucket):
 
 
 def upload_file(s3_bucket, path, key):
-    content_type = mimetypes.guess_type(key)[0] or 'text/plain'  # checks file type, default to plain
+    """Upload path to s3_bucket at key."""
+    # checks file type, default to plain
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    
     s3_bucket.upload_file(
         path,
         key,
         ExtraArgs={
-            'ContentType': 'text/html'
+            'ContentType': content_type
         })
 
 
@@ -93,17 +120,17 @@ def upload_file(s3_bucket, path, key):
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-    """Sync contents of PATHNAME to BUCKET"""
+    """Sync contents of PATHNAME to BUCKET."""
     s3_bucket = s3.Bucket(bucket)
     root = Path(pathname).expanduser().resolve()
-    
+
     def handle_directory(target):
         for p in target.iterdir():
             if p.is_dir():
                 handle_directory(p)
             if p.is_file():
                 upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-            
+
     handle_directory(root)
 
 
